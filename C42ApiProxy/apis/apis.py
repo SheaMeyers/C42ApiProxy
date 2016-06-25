2023957dcabdc3cff4af957fb48e6a7552439297
SheaMeyers <shea.meyers@gmail.com>
2016-06-25 17:31:12 -0600

    Have functions call to api and return a jsonified response.

---
+++ C42ApiProxy/apis/apis.py
@@ -0,0 +1,21 @@
+import requests
+
+headers = {'Authorization': 'Token a850fb228b367d3cb2f93814a7f5380ff0583142'}
+
+
+def get_event_details(event_id):
+
+    url = 'http://demo.calendar42.com/api/v2/events/' + event_id + '/'
+
+    resp = requests.get(url, headers=headers)
+
+    return resp.json()
+
+
+def get_event_subscriptions(event_id):
+
+    url = 'https://demo.calendar42.com/api/v2/event-subscriptions/?event_ids=[' + event_id + ']'
+
+    resp = requests.get(url, headers=headers)
+
+    return resp.json()
